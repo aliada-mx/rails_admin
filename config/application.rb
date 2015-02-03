c3bc50042393525fd218368d9d4a0ab677e444d9
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2015-02-03 17:14:08 -0600

    Remove 4.2 specific config setting

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,6 @@ module AliadaWebApp
     # config.i18n.default_locale = :de
     config.autoload_paths << Rails.root.join('lib')
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.generators do |g|
       g.test_framework :rspec
     end
