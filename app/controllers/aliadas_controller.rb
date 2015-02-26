a20a969a51b87c7980fbfc6495c3adde9427b282
Alejandro <alejandroteutli@me.com>
2015-02-25 18:34:16 -0600

    Added map to aliadas services dash

--- app/controllers/aliadas_controller.rb
+++ app/controllers/aliadas_controller.rb
@@ -7,12 +7,17 @@ class AliadasController < ApplicationController
     
     
     if @aliada 
-      @upcoming_services = Service.joins(:address).where(aliada_id: @aliada.id)
+      #must implement today or tomorrow after 6pm, etc...
+      date_to_show = if DateTime.now.hour < 19
+                       Date.today
+                     else
+                       Date.tomorrow
+                     end
+      @upcoming_services = Service.joins(:address).where(aliada_id: @aliada.id, :datetime => date_to_show.beginning_of_day..date_to_show.end_of_day).joins(:user)
+      
       @message = 'Token correct'
     else
-      render text: 'Ruta invalida, ponte en contacto con Aliada' + params[:token]
+      render text: 'Ruta invalida, ponte  en contacto con Aliada' + params[:token]
     end
-    
   end
-  
 end
--- spec/controllers/aliadas_controller_spec.rb
+++ spec/controllers/aliadas_controller_spec.rb
@@ -14,8 +14,9 @@ feature 'AliadasController' do
                          recurrence: recurrence,
                          zone: zone,
                          service_type: one_time_service,
-                         datetime: starting_datetime,
-                         billable_hours: 3) }
+                         datetime: starting_datetime 
+                         #billable_hours: 3
+) }
   before do
     Timecop.freeze(starting_datetime)
   end
@@ -26,9 +27,10 @@ feature 'AliadasController' do
 
 
   describe '#services' do
-    it 'checks the aliada is signed in' do
+    it 'checks the aliada has a valid token' do
       aliada = create(:aliada)
-      address1 = create(:address, city: 'Mexico',
+      client = create(:user, phone: '54545454', first_name: 'Juan', last_name:'Perez Tellez')
+      address1 = create(:address, city: 'Cuauhtemoc',
                        street: 'Tabasco', number: '232', 
                        interior_number: 'torre A 802',
                        between_streets: 'colima y tonala',
@@ -36,22 +38,64 @@ feature 'AliadasController' do
                        state: 'DF',
                        latitude: 19.98,
                        longitude: 20.45)
-      address2 = create(:address, city: 'Mexico',
-                       street: 'Tamal', number: '32', 
+      address2 = create(:address, city: 'Coyoacan',
+                       street: 'Tamales', number: '32', 
                        interior_number: '802',
                        between_streets: 'Insurgentes',
                        colony: 'Doctores',
-                       state: 'DF',
+                        state: 'DF',
                        latitude: 19.99,
                        longitude: 20.45) 
-      Servicio1 = create(:service, aliada_id: aliada.id, address_id: address1.id)
-      Servicio2 = create(:service, aliada_id: aliada.id, address_id: address2.id)
+      Servicio1 = create(:service, aliada_id: aliada.id, address_id: address1.id,
+                         bathrooms: 2,
+                         bedrooms: 3,
+                         user_id: client.id, datetime: DateTime.now
+                         )
+      Servicio2 = create(:service, aliada_id: aliada.id, address_id: address2.id,
+                         user_id: client.id, datetime: DateTime.now)
       #login_as(user)
-
+  
       visit  ('aliadas/servicios/'+ aliada.authentication_token)
       save_and_open_page
       page.has_content?('Tus servicios')
     end
+    
+    it 'Shows tomorrows services if it is tommorrow' do
+      aliada = create(:aliada)
+      client = create(:user, phone: '54545454', first_name: 'Juan', last_name:'Perez Tellez')
+      address1 = create(:address, city: 'Cuauhtemoc',
+                       street: 'Tabasco', number: '232', 
+                       interior_number: 'torre A 802',
+                       between_streets: 'colima y tonala',
+                       colony: 'Roma Norte',
+                       state: 'DF',
+                       latitude: 19.98,
+                       longitude: 20.45)
+      address2 = create(:address, city: 'Coyoacan',
+                       street: 'Tamales', number: '32', 
+                       interior_number: '802',
+                       between_streets: 'Insurgentes',
+                       colony: 'Doctores',
+                        state: 'DF',
+                       latitude: 19.99,
+                       longitude: 20.45) 
+      Servicio1 = create(:service, aliada_id: aliada.id, address_id: address1.id,
+                         bathrooms: 2,
+                         bedrooms: 3,
+                         user_id: client.id, datetime: (DateTime.now+1)
+                         )
+      Servicio2 = create(:service, aliada_id: aliada.id, address_id: address2.id,
+                         user_id: client.id, datetime: DateTime.now)
+      #login_as(user)
+      Timecop.freeze(Date.today + 1) do
+         visit  ('aliadas/servicios/'+ aliada.authentication_token)
+        save_and_open_page
+        expect(page).to   have_content 'Roma Norte'
+      end
+  
+     
+    end
+    
     
     it 'Shows message if token invalid' do
       visit  ('aliadas/servicios/'+ user.authentication_token)
