404dd29fb84c36b6da4ef776728251dad1e5fa51
Alejandro <alejandroteutli@me.com>
2015-03-04 08:16:04 -0600

    Cleaned up copy, comments

--- app/controllers/aliadas_controller.rb
+++ app/controllers/aliadas_controller.rb
@@ -1,8 +1,17 @@
 class AliadasController < ApplicationController
+  
+  def finish
+     @aliada = Aliada.find_by(authentication_token: params[:token])
+    @service_to_finish = Service.where(aliada_id: @aliada.id, status: 'aliada_assigned').where("datetime <= ?", DateTime.now).take
+    if @service_to_finish
+      
+    else
+      render text: 'Ruta invalida, ponte  en contacto con aliada' + params[:token]
+    end
+  end
 
   def services 
-    #this should be changed to aliada, but for debuggin it riffs
-    # save_and_open_pages
+   
     @aliada = Aliada.find_by(authentication_token: params[:token])
     
     
@@ -17,7 +26,7 @@ class AliadasController < ApplicationController
       @upcoming_services = Service.joins(:address).where(aliada_id: @aliada.id, :datetime => date_to_show.beginning_of_day..date_to_show.end_of_day).joins(:user)      
       @message = 'Token correct'
     else
-      render text: 'Ruta invalida, ponte  en contacto con Aliada' + params[:token]
+      render text: 'Ruta invalida, ponte  en contacto con aliada' + params[:token]
     end
   end
 end
