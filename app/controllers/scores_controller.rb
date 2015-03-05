37fcb9361579846191c7d65de0493d46b1b967c0
tonklis <benjamin.hernandez@in2teck.com>
2015-03-04 19:29:04 -0600

    Added rspec tests to Score. Also, fixed migration without PostalCodeZones

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,10 +1,12 @@
 class ScoresController < ApplicationController
+  
+  authorize_resource
 
   def create_by_service_id
     service = Service.find(params[:service_id])
     score = Score.create(user_id: service.user_id, value: params[:value], aliada_id: service.aliada_id, comment: params[:comment], service_id: params[:service_id])
     
-    return render json: { status: :success }
+    return render json: { status: :success, score: score }
 
   end
 
