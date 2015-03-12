63e3c0c3ef8dd622b59994dfcb8a1d141cd5875e
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2015-03-12 16:12:15 -0600

    Fix typo

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -104,6 +104,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+  }
 
   config.host = 'stagin.aliada.mx'
 end
