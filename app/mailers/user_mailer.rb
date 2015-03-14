4f977a6a96904e446356da5474ed57aafff32eef
Alejandro <alejandroteutli@me.com>
2015-03-13 20:53:53 -0600

    Implemented billing mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -17,7 +17,7 @@ class UserMailer < ApplicationMailer
     substitutions:
       {'-user_full_name-' => [ user.full_name  ],
       '-password_change_url-' => [ service.aliada.full_name],
-      '-password_change_request_at-' => [ 24 ]},
+      '-password_change_request_at-' => [ '' ]},
     template_id: template_id
   end
   
@@ -62,6 +62,13 @@ class UserMailer < ApplicationMailer
     template_id = Setting.sendgrid_templates_ids[:service_receipt]
     service = Service.where(id: service.id).joins(:address).joins(:service_type).joins(:aliada).first
     
+    hours = service.aliada_reported_end_time.hours - service.aliada_reported_begin_time.hour
+    minutes = service.aliada_reported_end_time.min - service.aliada_reported_begin_time.min
+    total_time = hours + minutes/60.0
+    hours = hours.floor
+    minutes = ((total_time - hours)*60).floor
+   
+
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [ user.full_name  ],
@@ -69,11 +76,11 @@ class UserMailer < ApplicationMailer
       '-service_time-' => [(I18n.l service.datetime, format: '%I %p')] ,
       '-service_address-' => [service.address.full_address],
       '-aliada_reported_begin_time-' =>[(I18n.l service.aliada_reported_begin_time, format: '%H:%M %p')],
-      '-aliada_reported_end_time-' =>[(I18n.l service.aliada_reported_begin_time, format: '%H:%M %p')],
-      '-service_friendly_total_hours-' =>[],
-      '-service_amount_to_bill' => [],
-      '-service_subtotal-' => [],
-      '-aliada_full_name-' => [ service.aliada.full_name]},
+      '-aliada_reported_end_time-' =>[(I18n.l service.aliada_reported_end_time, format: '%H:%M %p')],
+      '-service_friendly_total_hours-' =>["#{hours} hrs con #{minutes} minutos"],
+      '-service_amount_to_bill' => [service.price],
+      '-service_subtotal-' => [service.amount_to_bill],
+      '-aliada_full_name-' => [service.aliada.full_name]},
     template_id: template_id
     
   end
@@ -84,16 +91,12 @@ class UserMailer < ApplicationMailer
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [ user.full_name  ],
-     
       '-service_payment_last_4-' => [ payment_method.last4]},
     template_id: template_id
   end
 
   def user_address_changed(user,new_address, prev_address)
     template_id = Setting.sendgrid_templates_ids[:change_client_address]
-    
-    
-    
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [ user.full_address],
