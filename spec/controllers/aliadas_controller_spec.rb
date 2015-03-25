a054a226b1a4a4f38e9a71e800d7903e8076390b
Alejandro <alejandroteutli@me.com>
2015-03-24 19:57:13 -0600

    Fixed aliadas_controller_spec test time usage

--- spec/controllers/aliadas_controller_spec.rb
+++ spec/controllers/aliadas_controller_spec.rb
@@ -101,7 +101,7 @@ feature 'AliadasController' do
       
       Timecop.freeze('2015-09-04') do
         visit  ('aliadas/servicios/'+ aliada.authentication_token)
-        save_and_open_page
+        #save_and_open_page
         expect(page).to   have_content 'Roma Norte'
       end
       
