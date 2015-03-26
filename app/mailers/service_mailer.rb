d29027b131efe8a8b39db17b9eaf5d2dbe31ea2a
Alejandro <alejandroteutli@me.com>
2015-03-26 17:41:45 -0600

    Service mailer fully functional

--- app/mailers/service_mailer.rb
+++ app/mailers/service_mailer.rb
@@ -1,14 +1,15 @@
 class ServiceMailer < ApplicationMailer
+include  ActionView::Helpers::DateHelper
 
   def aliada_changed(service)
     template_id = Setting.sendgrid_templates_ids[:aliada_changed]
-     service = Service.where(id: service.id).joins(:users).joins(:service_type).joins(:aliada).first
+     service = Service.where(id: service.id).joins(:user).joins(:service_type).first
     
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [ service.user.full_name  ],
-      '-weekday-' => [(I18n.l service.datetime, format: '%A %d')],
-      '-time-' => [(I18n.l service.datetime, format: '%I %p')],
+      '-weekday-' => [(I18n.l service.tz_aware_datetime, format: '%A %d')],
+      '-time-' => [(I18n.l service.tz_aware_datetime, format: '%I %p')],
       '-service_type_name-' => [service.service_type.display_name],
       '-aliada_full_name-' => [ service.aliada.full_name],
       '-aliada_phone-' => [service.aliada.phone]},
@@ -17,13 +18,13 @@ class ServiceMailer < ApplicationMailer
   
   def hour_changed(service)
     template_id = Setting.sendgrid_templates_ids[:hour_changed]
-     service = Service.where(id: service.id).joins(:users).joins(:service_type).joins(:aliada).first
+     service = Service.where(id: service.id).joins(:user).joins(:service_type).first
     
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [ service.user.full_name  ],
-      '-service_friendly_datetime-' => [(I18n.l service.datetime, format: '%A %d')],
-      '-service_current_hour-' => [(I18n.l service.datetime, format: '%I %p')] ,
+      '-service_friendly_datetime-' => [(I18n.l service.tz_aware_datetime, format: '%A %d')],
+      '-service_current_hour-' => [(I18n.l service.tz_aware_datetime, format: '%I %p')] ,
       '-service_address-' => [service.address.full_address],
       '-service_type_name-' => [service.service_type.display_name],
       '-aliada_full_name-' => [ service.aliada.full_name],
@@ -35,13 +36,13 @@ class ServiceMailer < ApplicationMailer
   def untimely_cancelation(service)
     template_id = Setting.sendgrid_templates_ids[:untimely_cancelation]
     
-    service = Service.where(id: service.id).joins(:users).joins(:address).joins(:service_type).joins(:aliada).first
+    service = Service.where(id: service.id).joins(:user).joins(:address).joins(:service_type).first
     
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [service.user.full_name],
-      '-service_friendly_datetime-' => [(I18n.l service.datetime, format: '%A %d')],
-      '-service_current_hour-' => [(I18n.l service.datetime, format: '%I %p')] ,
+      '-service_friendly_datetime-' => [(I18n.l service.tz_aware_datetime, format: '%A %d')],
+      '-service_current_hour-' => [(I18n.l service.tz_aware_datetime, format: '%I %p')] ,
       '-service_address-' => [service.address.full_address],
       '-service_type_name-' => [service.service_type.display_name],
       '-aliada_full_name-' => [ service.aliada.full_name],
@@ -53,13 +54,12 @@ class ServiceMailer < ApplicationMailer
   def address_changed(service, previous_address)
     template_id = Setting.sendgrid_templates_ids[:change_service_address]
     
-    service = Service.where(id: service.id).joins(:users).joins(:service_type).joins(:aliada)
-    
+    service = Service.where(id: service.id).joins(:user).joins(:service_type).first
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [ service.user.full_name  ],
-      '-service_date-' => [(I18n.l service.datetime, format: '%A %d')],
-      '-service_hour-' => [(I18n.l service.datetime, format: '%I %p')] ,
+      '-service_date-' => [(I18n.l service.tz_aware_datetime, format: '%A %d')],
+      '-service_time-' => [(I18n.l service.tz_aware_datetime, format: '%I %p')] ,
       '-current_address-' => [service.address.full_address],
       '-service_type_name-' => [service.service_type.display_name],
       '-previous_address-' => [previous_address.full_address]},
@@ -70,13 +70,13 @@ class ServiceMailer < ApplicationMailer
   
   def timely_cancelation(service)
     template_id = Setting.sendgrid_templates_ids[:timely_cancelation]
-    service = Service.where(id: service.id).joins(:users).joins(:address).joins(:service_type).joins(:aliada).first
+    service = Service.where(id: service.id).joins(:user).joins(:address).joins(:service_type).first
     
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [service.user.full_name],
-      '-service_date-' => [(I18n.l service.datetime, format: '%A %d')],
-      '-service_time-' => [(I18n.l service.datetime, format: '%I %p')] ,
+      '-service_date-' => [(I18n.l service.tz_aware_datetime, format: '%A %d')],
+      '-service_time-' => [(I18n.l service.tz_aware_datetime, format: '%I %p')] ,
       '-service_address-' => [service.address.full_address],
       '-service_type_name-' => [service.service_type.display_name]},
     template_id: template_id
@@ -84,14 +84,18 @@ class ServiceMailer < ApplicationMailer
 
   def reminder(service)
     template_id = Setting.sendgrid_templates_ids[:reminder]
-    service = Service.where(id: service.id).joins(:users).joins(:address).joins(:service_type).joins(:aliada).first
+    service = Service.where(id: service.id).joins(:user).joins(:address).joins(:service_type).first
     
     sendgrid_template_mail to: 'alex@aliada.mx',
     substitutions:
       {'-user_full_name-' => [service.user.full_name],
-      '-service_date-' => [(I18n.l service.datetime, format: '%A %d')],
-      '-service_time-' => [(I18n.l service.datetime, format: '%I %p')] ,
+      '-aliada_full_name-'=> [service.aliada.full_name],
+      '-aliada_phone-'=>[service.aliada.phone],
+      '-service_date-' => [(I18n.l service.tz_aware_datetime, format: '%A %d')],
+      '-service_time-' => [(I18n.l service.tz_aware_datetime, format: '%I %p')] ,
       '-service_address-' => [service.address.full_address],
+      '-late_cancelation_fee-' => ["$100"],
+      '-service_future_reference-' => [distance_of_time_in_words(service.datetime, Time.now)],
       '-service_type_name-' => [service.service_type.display_name]},
     template_id: template_id
   end
