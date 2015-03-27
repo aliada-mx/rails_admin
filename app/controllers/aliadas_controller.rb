0d2dfaad44c9ded933905c07f8bb2464abf917fd
Alejandro <alejandroteutli@me.com>
2015-03-26 20:29:56 -0600

    Fixed amount to bill to consider minimum hours

--- app/controllers/aliadas_controller.rb
+++ app/controllers/aliadas_controller.rb
@@ -3,7 +3,9 @@ class AliadasController < ApplicationController
   def finish
     
     @aliada = Aliada.find_by(authentication_token: params[:token])
-    @service_to_finish = Service.where(id: params[:service],  aliada_id: @aliada.id, status: 'aliada_assigned').where("datetime <= ?", DateTime.now.utc).take
+    @service_to_finish = Service.where(id: params[:service], 
+                                       aliada_id: @aliada.id, 
+                                       status: 'aliada_assigned').where("datetime <= ?", DateTime.now.utc).take
     if @service_to_finish
       @service_to_finish.aliada_reported_begin_time =  ActiveSupport::TimeZone["Mexico City"].parse(params[:begin_time])
       @service_to_finish.aliada_reported_end_time = ActiveSupport::TimeZone["Mexico City"].parse(params[:end_time])
@@ -22,17 +24,17 @@ class AliadasController < ApplicationController
     
     if @aliada 
       #must implement today or tomorrow after 6pm, etc...
-      now = Time.zone.now
+      now = ActiveSupport::TimeZone["Mexico City"].now
       date_to_show = if now.hour < Setting.show_tomorrow_services_cutoff
-                       Time.zone.today
+                       ActiveSupport::TimeZone["Mexico City"].today 
                      else
-                       Time.zone.today + 1.day
+                       ActiveSupport::TimeZone["Mexico City"].today + 1.day
                      end
      
       @unfinished_services = Service.joins(:user).where(aliada_id: @aliada.id, status: 'aliada_assigned').where("datetime <= ?", now.utc)
       @upcoming_services = Service.joins(:address).where(aliada_id: @aliada.id, :datetime => date_to_show.beginning_of_day..date_to_show.end_of_day).not_canceled.joins(:user) 
       
-      @message = 'Token correct'
+      @message = "Token correct"
     else
       render text: 'Ruta invalida, ponte  en contacto con aliada' + params[:token]
     end
