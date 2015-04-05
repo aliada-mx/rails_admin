6e06470dda73343f4acb4dfb67c6abfb0f6619a3
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2015-04-04 21:18:11 -0600

    Add unaccented search to services

--- app/models/unscoped_user.rb
+++ app/models/unscoped_user.rb
@@ -1,7 +1,10 @@
 class UnscopedUser < User
   scope :with_name_phone_email, -> (query) do
     query = "%#{query}%"
-    where('users.first_name ILIKE ? OR users.last_name ILIKE ? OR users.email ILIKE ? OR users.phone ILIKE ?', *[ query ]*4)
+    where('unaccent(users.first_name) ILIKE ? OR 
+           unaccent(users.last_name) ILIKE ? OR 
+           users.email ILIKE ? OR 
+           users.phone ILIKE ?', *[ query ]*4)
   end
 
   def self.default_scope
