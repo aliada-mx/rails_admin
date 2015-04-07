bd22df81bf409bd247be84604f2ca074b183b52f
Alejandro <alejandroteutli@me.com>
2015-04-06 19:13:57 -0500

    Changed to one more hour before end

--- app/helpers/aliada_helper.rb
+++ app/helpers/aliada_helper.rb
@@ -8,11 +8,11 @@ module AliadaHelper
 
 
    def end_hours_range(service)
-     end_hours_minimum = service.estimated_hours - 1
-     if end_hours_minimum < 3
-     then
-       end_hours_minimum = 3
-     end
+     end_hours_minimum = service.estimated_hours - 2
+    # if end_hours_minimum < 3
+    # then
+    #   end_hours_minimum = 3
+    # end
     
 
      range_begin =  (service.tz_aware_datetime + end_hours_minimum.hour).to_i
