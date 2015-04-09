72fd8ea714fd408cc2f12de90cfb90b63f4e7a7e
Alejandro <alejandroteutli@me.com>
2015-04-08 20:01:24 -0500

    Migration for aliada_reported_hours, changed to datetime, will change incorrect billable_hours

--- db/migrate/20150407233814_change_reported_times_to_datetime.rb
+++ db/migrate/20150407233814_change_reported_times_to_datetime.rb
@@ -5,20 +5,27 @@ class ChangeReportedTimesToDatetime < ActiveRecord::Migration
   end
   
   def down
-    
+    change_column :services, :aliada_reported_begin_time, "time USING aliada_reported_begin_time"
+    change_column :services, :aliada_reported_end_time, "time USING aliada_reported_end_time"
   end
   
   def data
     ###Here we fix the case where the aliada_end_time is before_the_begin_time
     services = Service.where("aliada_reported_end_time < aliada_reported_begin_time")
-    
+
     services.each do |s|
-      #swap
-      begin_time = s.aliada_reported_begin_time
-      end_time = s.aliada_reported_end_time
-      
-      s.aliada_reported_begin_time = end_time
-      s.aliada_reported_end_time = begin_time
+      #When the times get fucked up, we straighten them
+      puts "#{s.aliada_reported_end_time} #{s.user_id}"
+      end_time = s.aliada_reported_end_time + 1.day
+      s.aliada_reported_end_time = end_time
+      puts "#{s.aliada_reported_end_time} #{s.user_id}"
+      s.save!
+    end
+    
+    #Arregla los billable hours,
+    services = Service.where("billable_hours < 0").where(status: 'finished')
+    services.each do |s|
+      s.billable_hours = s.reported_hours
       s.save!
     end
   end
