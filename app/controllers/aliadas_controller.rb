8d2b55f7dc65733cc45a1d318d23101c8cdd8438
Alejandro <alejandroteutli@me.com>
2015-04-19 21:36:50 -0500

    Removed binding.pry

--- app/controllers/aliadas_controller.rb
+++ app/controllers/aliadas_controller.rb
@@ -19,7 +19,7 @@ class AliadasController < ApplicationController
       hours = params[:hour].to_i
       min = params[:min].to_i
       hours = hours + (min/60.0)
-      binding.pry
+      
       @service_to_finish.hours_worked = hours
      # @service_to_finish.aliada_reported_begin_time = ActiveSupport::TimeZone["Mexico City"].parse("#{params[:begin_hour]}:#{params[:begin_min]}")
      # @service_to_finish.aliada_reported_end_time = ActiveSupport::TimeZone["Mexico City"].parse("#{params[:end_hour]}:#{params[:end_min]}")
