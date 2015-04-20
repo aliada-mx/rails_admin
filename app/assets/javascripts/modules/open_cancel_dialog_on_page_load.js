62f6458997f89527f2b41f936cf4e7c0fde8b7b5
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2015-04-20 16:20:11 -0500

    Checkbox to toggle all in aliada working hours, fix cancelar

--- app/assets/javascripts/modules/open_cancel_dialog_on_page_load.js
+++ app/assets/javascripts/modules/open_cancel_dialog_on_page_load.js
@@ -1,6 +1,6 @@
 aliada.open_cancel_dialog_on_page_load = function(){
   var url = new URI(window.location.href);
-  var should_open_cancel_dialog = url.hasQuery('cancel', true);
+  var should_open_cancel_dialog = url.hasQuery('cancelar', true);
 
   if ( should_open_cancel_dialog ){
     window.setTimeout(function(){
