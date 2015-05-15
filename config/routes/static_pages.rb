1b1afa6e7634c32f717fd37c6956dfcc15c4f4c0
Rogelio Alberto <i@rog.mx>
2015-05-15 02:35:53 -0500

    Fix merge of routes and adds jobs route

--- config/routes/static_pages.rb
+++ config/routes/static_pages.rb
@@ -6,5 +6,6 @@ get 'faq', to: 'static_pages#faq', as: :faq
 get 'terminos', to: 'static_pages#terms', as: :terms
 get 'privacidad', to: 'static_pages#privacy', as: :privacy
 get 'patrones', to: 'static_pages#pattern_dictionary'
+get 'jobs', to: 'static_pages#jobs', as: :jobs
 get 'reclutamiento', to: 'static_pages#recruitment', as: :recruitment
 get 'reclutamiento/registro', to: 'static_pages#recruitment_signup', as: :recruitment_signup
