b666aa5d13a9fc7d590991b206ee0f48b9a85185
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2015-05-18 16:06:29 -0500

    Move routes patch to own file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 # -*- encoding : utf-8 -*-
-class ActionDispatch::Routing::Mapper
-  def draw(routes_name)
-    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
-  end
-end
-
 Rails.application.routes.draw do
   # Admin
   mount RailsAdmin::Engine => 'aliadadmin', as: 'rails_admin'
