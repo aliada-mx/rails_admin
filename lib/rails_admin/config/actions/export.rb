82f0ec437cb652847ee4d71b8989882b6868686c
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-09-14 18:39:56 -0500

    Exports the model data in a child process and sends the report by email

--- lib/rails_admin/config/actions/export.rb
+++ lib/rails_admin/config/actions/export.rb
@@ -14,11 +14,33 @@ module RailsAdmin
 
         register_instance_option :controller do
           proc do
-            if format = params[:json] && :json || params[:csv] && :csv || params[:xml] && :xml
-              request.format = format
-              @schema = params[:schema].symbolize if params[:schema] # to_json and to_xml expect symbols for keys AND values.
-              @objects = list_entries(@model_config, :export)
-              index
+            if (format = params[:json] && :json || params[:csv] && :csv || params[:xml] && :xml)
+              config = ActiveRecord::Base.remove_connection
+              pid = fork do
+                begin
+                  ActiveRecord::Base.establish_connection(config)
+                  @schema = params[:schema].symbolize if params[:schema] # to_json and to_xml expect symbols for keys AND values.
+                  @objects = list_entries(@model_config, :export)
+
+                  case format
+                  when :json
+                    attachment_data = @objects.to_json(@schema)
+                  when :xml
+                    attachment_data = @objects.to_xml(@schema)
+                  when :csv
+                    _, _, attachment_data = CSVConverter.new(@objects, @schema).to_csv(params[:csv_options])
+                  end
+
+                  attachment_filename = "#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.#{format}"
+                  ExportMailer.results_email(current_user, attachment_filename, attachment_data).deliver_now
+                ensure
+                  ActiveRecord::Base.remove_connection
+                  Process.exit!(0)
+                end
+              end
+              Process.detach(pid)
+              ActiveRecord::Base.establish_connection(config)
+              redirect_to index_path
             else
               render @action.template_name
             end
--- lib/rails_admin/config/actions/index.rb
+++ lib/rails_admin/config/actions/index.rb
@@ -43,43 +43,6 @@ module RailsAdmin
               format.html do
                 render @action.template_name, status: (flash[:error].present? ? :not_found : 200)
               end
-
-              format.json do
-                output = begin
-                  if params[:compact]
-                    primary_key_method = @association ? @association.associated_primary_key : @model_config.abstract_model.primary_key
-                    label_method = @model_config.object_label_method
-                    @objects.collect { |o| {id: o.send(primary_key_method).to_s, label: o.send(label_method).to_s} }
-                  else
-                    @objects.to_json(@schema)
-                  end
-                end
-                if params[:send_data]
-                  send_data output, filename: "#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.json"
-                else
-                  render json: output, root: false
-                end
-              end
-
-              format.xml do
-                output = @objects.to_xml(@schema)
-                if params[:send_data]
-                  send_data output, filename: "#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.xml"
-                else
-                  render xml: output
-                end
-              end
-
-              format.csv do
-                header, encoding, output = CSVConverter.new(@objects, @schema).to_csv(params[:csv_options])
-                if params[:send_data]
-                  send_data output,
-                            type: "text/csv; charset=#{encoding}; #{'header=present' if header}",
-                            disposition: "attachment; filename=#{params[:model_name]}_#{DateTime.now.strftime('%Y-%m-%d_%Hh%Mm%S')}.csv"
-                else
-                  render text: output
-                end
-              end
             end
           end
         end
